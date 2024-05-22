8230bb60d11a3c7b9e3b2030a733748e28d9908b
wajeeha-arain <arainwajeeha715@gmail.com>
2024-05-22 09:37:29 +0500

    new add 1

---
+++ index.js
@@ -0,0 +1,156 @@
+// null( value a sign)
+
+// compreision operator
+
+
+// let num = +prompt("what is num");
+// console.log(num +5);
+
+// let num1 = 6;
+// let num2 = 6;
+// console.log(num1 == num2);
+
+// let num3 = 6;
+// let num4 = 6;
+// console.log(num1 === num2);
+// ture
+
+// let num3 = 7;
+// let num4 = 6;
+// console.log(num1 === num2);
+// fales
+
+// compreision operator
+//!= not equal to   , ==!equal to
+
+
+//< greater than
+// let num = 55;
+// let num2 = 66;
+// console.log(num < num2);
+
+// >less than
+//console.log(5 > 7);
+
+// let num = 155;
+// let num2 = 44;
+// console.log(num > num2);
+// ture
+
+// <==
+// greater than
+// <=
+// let num = 155;
+// let num2 = 44;
+// console.log(num >= num2);
+// ture
+
+// let num = 155;
+// let num2 = 166;
+// console.log(num <= num2);
+// ture
+
+// let num = 155;
+// let num2 = 154;
+// console.log(num <= num2);
+// fales
+
+//condition
+
+// let age = +prompt("inter your age");
+// if( age >= 18){ 
+//     console.log("you can apply cnic")
+// }else{
+//     console.log("you are not eligible")
+// }
+
+
+
+// let userName = +prompt("inter your number");
+// let computerNumber = Math.round(Math.random() * 5);
+// if(userName === computerNumber){
+//     console.log("you win")
+// }else{
+//     console.log(" you loss ! "  + computerNumber )
+// }
+
+ //prompt show  by default value 'string'
+
+//......................  class -4  ...............//
+
+// console.log(5 != 7);
+
+// let ice = +prompt("inter your number");
+// if(ice === 100){
+//     console.log("ice-Cream is corenetto ");
+
+// }else if(ice === 500){
+//     console.log(".ice-Cream is corenetto family pack 1.5 LTR");
+// }else if(ice === 1000){
+//     console.log(".ice-Cream is corenetto family pack 2.5 LTR");
+// }else{
+//     console.log("your ice-Cearm is random ");
+// }
+
+
+// let ice = +prompt("what is your items")
+// switch (ice) {
+//     case 100:
+//            console.log("ice-Cream is corenetto ");
+//            break;
+//         case 500:
+//             console.log(".ice-Cream is corenetto family pack 1.5 LTR");
+//             break;
+//          case 1000:
+//             console.log(".ice-Cream is corenetto family pack 2.5 LTR");
+//             break;
+//     default:
+//         console.log("your ice-Cearm is random ");
+//         break;
+// }
+
+
+// let usernum = prompt("Even num");
+// let compNumber = Math.round(Math.random( ) * 10);
+
+// if(compNumber % 2 === 0 && usernum === "even" ){
+//     console.log(" you win ! "  + compNumber);
+// }else if( compNumber % 2 !== 0  && usernum === "odd"){
+//     console.log(" you win ! "  + compNumber);
+// }else{
+//     console.log(" you loss! "  + compNumber);
+// }
+
+//......................'loop'............................///
+//loop
+//initialization
+//condition
+//increment
+// for(let i = 0 ; i <= 10 ; i ++){
+//     console.log(" hello world");
+// }
+
+// for(let i = 1 ; i <= 100 ; i ++){
+//          console.log(" hello world" + i);
+//      }
+
+// for(let i = 1 ; i <= 10 ; i ++){
+//     console.log("2 x " + i + " = " + 2 *i );
+// }
+
+// let table = 9;
+// for(let i = 1 ; i <= 10 ; i++){
+//     console.log( table + " x " + i + " = " + table *i );
+// }
+
+
+// let table = 9;
+//  for(let i = 1 ; i <= 10 ; i++){
+//     console.log( table + " x " + i + " = " + table *i );
+// console.log( Math.round(Math.random( ) * 10));
+// console.log("hello world")
+
+// }
+
+
+
